3253ede3e31f82b5ceeed5f36b098cf344aed91a
oliwia <oliwia.giercza@gmail.com>
2023-07-24 17:59:49 -0500

    added backing properties to currentScrambledWord and count

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,7 +3,11 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    private var score = 0
-    private var currentWordCount = 0
-    private var currentScrambledWord = "test"
+    private var _score = 0
+    private var _currentWordCount = 0
+    private var _currentScrambledWord = "test"
+    val score: Int
+        get() = _score
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 }
