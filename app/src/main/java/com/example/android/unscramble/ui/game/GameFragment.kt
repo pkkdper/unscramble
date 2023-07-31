95da5796252e6a27086335a99373509b0f4d4e16
oliwia <oliwia.giercza@gmail.com>
2023-07-31 08:54:19 -0500

    finished app (no updates on score yet)

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -116,6 +116,7 @@ class GameFragment : Fragment() {
      * restart the game.
      */
     private fun restartGame() {
+        viewModel.reinitializeData()
         setErrorTextField(false)
         updateNextWordOnScreen()
     }
