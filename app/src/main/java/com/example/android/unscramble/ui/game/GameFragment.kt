50e2559e46d46cba022b1f291f3aa579e22cf408
oliwia <oliwia.giercza@gmail.com>
2023-08-01 09:45:44 -0500

    added observer for curentScrambledWord

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -67,6 +67,8 @@ class GameFragment : Fragment() {
         binding.score.text = getString(R.string.score, 0)
         binding.wordCount.text = getString(
                 R.string.word_count, 0, MAX_NO_OF_WORDS)
+        // Observe the currentScrambledWord LiveData.
+        viewModel.currentScrambledWord.observe()
     }
 
     /*
